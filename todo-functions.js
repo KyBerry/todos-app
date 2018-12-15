a9baba89b30694d2efaf7d2bef1e21c0b1296dc5
Kyle Berry <kyleberry@Kyles-MacBook-Pro.local>
2018-12-15 10:31:17 -0700

    adds new file containing refactored code

---
+++ todo-functions.js
@@ -0,0 +1,48 @@
+// Fetch existing todos from localStorage
+const getSavedTodos = function(){
+    const todoJSON = localStorage.getItem('todos');
+    if(todoJSON !== null){
+        return JSON.parse(todoJSON);
+    } else {
+        return [];
+    }
+}
+
+// Save todos to localStorage
+const saveTodos = function(todos){
+   localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// Render application todos based on filters
+const renderTodos = function(todos, filters){
+    const filteredTodos = todos.filter(function(todo){
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+        return searchTextMatch && hideCompletedMatch;
+    })
+    const incompleteTodos = filteredTodos.filter(function(todo){
+        return !todo.completed
+      });
+
+    document.querySelector('#todos').innerHTML = '';
+    document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos));
+
+    // Add a p for each todo above (use text value
+    filteredTodos.forEach(function(todo){
+        document.querySelector('#todos').appendChild(generateTodoDOM(todo));
+    })
+}
+
+// Get the DOM elements for an individual todo
+const generateTodoDOM = function(todo){
+        const p = document.createElement('p');
+        p.textContent = todo.text;
+        return p;
+}
+
+// Get the DOM elements for an individual note
+const generateSummaryDOM = function(incompleteTodos){
+    const todosLeft = document.createElement('h2');
+    todosLeft.textContent = `You have ${incompleteTodos.length} todos left`
+    return todosLeft;
+}
